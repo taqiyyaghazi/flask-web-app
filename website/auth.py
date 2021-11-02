70dc594a3ae0e7e2e0edb454605f7e7c207c4b4e
Taqiyya Ghazi <taqiyyaghazi@gmail.com>
2021-11-02 13:07:33 +0700

    Handling POST Request

--- website/auth.py
+++ website/auth.py
@@ -1,8 +1,9 @@
-from flask import Blueprint, render_template
+import re
+from flask import Blueprint, render_template, request
 
 auth = Blueprint('auth', __name__)
 
-@auth.route('/login')
+@auth.route('/login', methods=['GET', 'POST'])
 def login():
     return render_template('login.html')
 
@@ -10,6 +11,22 @@ def login():
 def logout():
     return '<a href="/">Logout</a>'
 
-@auth.route('sign-up')
+@auth.route('sign-up', methods=['GET', 'POST'])
 def sign_up():
+    if request.method == 'POST':
+        email = request.form.get('email')
+        firstName = request.form.get('firstName')
+        password1 = request.form.get('password1')
+        password2 = request.form.get('password2')
+
+        if len(email) < 4:
+            pass
+        elif len(firstName) < 2:
+            pass
+        elif password1 != password2:
+            pass
+        elif len(password1) < 1:
+            pass
+        else:
+            pass #add user to database
     return render_template('sign_up.html')
