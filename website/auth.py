00c21b8eff3e7642e703a5ef0f70c0123f4fb5cf
Taqiyya Ghazi <taqiyyaghazi@gmail.com>
2021-11-02 20:42:36 +0700

    Mengecek apakah user sedang login - Menampilkan menu beranda dan keluar hanya saat telah login (auth.py, base.html, views.html)

--- website/auth.py
+++ website/auth.py
@@ -22,7 +22,7 @@ def login():
         else:
             flash('Email belum terdaftar.', category='error')
 
-    return render_template('login.html')
+    return render_template('login.html', user=current_user)
 
 @auth.route('/logout')
 @login_required
@@ -56,4 +56,4 @@ def sign_up():
             login_user(user, remember=True) #Mengingat pengguna yang masuk
             flash('Akun telah dibuat.', category='succes') #add user to database
             return redirect(url_for('views.home'))
-    return render_template('sign_up.html')
+    return render_template('sign_up.html', user=current_user)
