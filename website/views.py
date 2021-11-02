6491274c0ecaf04d7f96db5834fb36bc2df81726
Taqiyya Ghazi <taqiyyaghazi@gmail.com>
2021-11-02 23:00:58 +0700

    Menambah fitur hapus catatan (index.js, home.html, login.html, sign_up.html, views.py)

--- website/views.py
+++ website/views.py
@@ -1,10 +1,37 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, flash, jsonify
+from flask.helpers import flash
 from flask_login import login_required, current_user
 from sqlalchemy.sql.functions import user
+from .models import Note
+from . import db
+import json
 
 views = Blueprint('views', __name__)
 
-@views.route('/')
+@views.route('/', methods=['GET', 'POST'])
 @login_required
 def home():
+    if request.method == 'POST':
+        note = request.form.get('note')
+
+        if len(note) <= 1:
+            flash('Catatan terlalu pendek!', category='error')
+        else:
+            new_note = Note(data=note, user_id=current_user.id)
+            db.session.add(new_note)
+            db.session.commit()
+            flash('Catatan telah ditambahkan.', category='success')
+
     return render_template('home.html', user=current_user) # Route Homepage
+
+@views.route('/delete-note', methods=['POST'])
+def delete_note():
+    note = json.loads(request.data)
+    noteId = note['noteId']
+    note = Note.query.get(noteId)
+    if note:
+        if note.user_id == current_user.id:
+            db.session.delete(note)
+            db.session.commit()
+
+    return jsonify({})
